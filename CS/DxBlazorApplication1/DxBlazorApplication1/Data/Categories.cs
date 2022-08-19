37320a4ed551f89f6464b77dd357ae80e0089f2b
Dan Valeroso <danlestervaleroso@gmail.com>
2022-08-20 00:00:07 +0800

    Add project

---
+++ CS/DxBlazorApplication1/DxBlazorApplication1/Data/Categories.cs
@@ -0,0 +1,16 @@
+﻿namespace DxBlazorApplication1.Data
+{
+    public class Categories
+    {
+        public Categories()
+        {
+            Products = new HashSet<Products>();
+        }
+
+        public int categoryId { get; set; }
+        public string categoryName { get; set; }
+        public string description { get; set; }
+
+        public virtual ICollection<Products> Products { get; set; }
+    }
+}
---
+++ CS/DxBlazorApplication1/DxBlazorApplication1/Data/Products.cs
@@ -0,0 +1,19 @@
+﻿namespace DxBlazorApplication1.Data
+{
+    public class Products
+    {
+        public int productId { get; set; }
+        public string productName { get; set; }
+        public int? supplierId { get; set; }
+        public int? categoryId { get; set; }
+        public string quantityPerUnit { get; set; }
+        public decimal? unitPrice { get; set; }
+        public short? unitsInStock { get; set; }
+        public short? unitsOnOrder { get; set; }
+        public short? reorderLevel { get; set; }
+        public bool discontinued { get; set; }
+        public string ean13 { get; set; }
+
+        public virtual Categories category { get; set; }
+    }
+}
