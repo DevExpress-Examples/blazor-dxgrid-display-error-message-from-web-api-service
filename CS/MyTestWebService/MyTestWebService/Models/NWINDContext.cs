9ae03286eafb8f42ee6a0a688bbd13d120698181
SvetlanaMikheeva <80316217+SvetlanaMikheeva@users.noreply.github.com>
2023-06-30 15:09:53 +0400

    update the database

--- CS/MyTestWebService/MyTestWebService/Models/NWINDContext.cs
+++ CS/MyTestWebService/MyTestWebService/Models/NWINDContext.cs
@@ -20,10 +20,6 @@ namespace MyTestWebService.Models
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            if (!optionsBuilder.IsConfigured)
-            {
-                optionsBuilder.UseSqlServer(@"Data Source=.;Initial Catalog=Northwind;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
-            }
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
